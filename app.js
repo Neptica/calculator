8facbd9d9af333d4e3ce0061f849015f5b85b834
Michael Mumme <neptica@hotmail.com>
2024-10-04 00:48:33 -0500

    add calculator functionality
    I think the only thing I need to do is fix the use of negatives

--- app.js
+++ app.js
@@ -1,12 +1,66 @@
 const controls = [
-  ["", "", "^", "/"],
-  ["7", "8", "9", "x"],
+  ["Clear", "", "^", "/"],
+  ["7", "8", "9", "*"],
   ["4", "5", "6", "-"],
   ["1", "2", "3", "+"],
   ["0", ".", "(-)", "Enter"],
 ];
 
+const operators = ["^", "/", "*", "-", "+"];
+
 const buttons = document.getElementById("button__container");
+const display = document.getElementById("display__text");
+let newString = true;
+
+// TODO: Work with negative numbers
+function updateDisplay() {
+  const input = this.textContent;
+  if (input == "Enter") {
+    display.textContent = doMath(display.textContent);
+    newString = true;
+  } else if (input === "Clear") {
+    display.textContent = "";
+    newString = false;
+  } else if (
+    operators.includes(input) &&
+    operators.some((item) => display.textContent.includes(item))
+  ) {
+    display.textContent = doMath(display.textContent) + input;
+    newString = false;
+  } else if (newString && !operators.includes(input)) {
+    display.textContent = input;
+    newString = false;
+  } else {
+    display.textContent += input;
+    newString = false;
+  }
+}
+
+function doMath(string) {
+  const op = operators.filter((item) => string.includes(item))[0];
+  const loc = string.indexOf(op);
+  const operand1 = Number(string.slice(0, loc));
+  const operand2 = Number(string.slice(loc + 1));
+  let answer = 0;
+  switch (op) {
+    case "+":
+      answer = operand1 + operand2;
+      break;
+    case "-":
+      answer = operand1 - operand2;
+      break;
+    case "*":
+      answer = operand1 * operand2;
+      break;
+    case "/":
+      answer = operand1 / operand2;
+      break;
+    case "^":
+      answer = operand1 ** operand2;
+      break;
+  }
+  return answer;
+}
 
 for (const row of controls) {
   let rowDoc = document.createElement("div");
@@ -15,6 +69,7 @@ for (const row of controls) {
     let btn = document.createElement("button");
     btn.classList.add("button");
     btn.textContent = ctrl;
+    btn.addEventListener("click", updateDisplay);
     rowDoc.appendChild(btn);
   }
   buttons.appendChild(rowDoc);
